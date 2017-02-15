a437f05df160fd142d4c6a3b865206ed3b6c4ccc
rarnu <hexiaojie@hujiang.com>
2017-02-16 01:00:06 +0800

    * add menu

--- src/main/kotlin/com/rarnu/tophighlight/xposed/HookSettings.kt
+++ src/main/kotlin/com/rarnu/tophighlight/xposed/HookSettings.kt
@@ -3,7 +3,11 @@ package com.rarnu.tophighlight.xposed
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import android.widget.TextView
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
 
@@ -11,36 +15,36 @@ import de.robv.android.xposed.XposedHelpers
  * Created by rarnu on 1/31/17.
  */
 object HookSettings {
-
-    var _ctx: Context? = null
-
+    
     fun hookSettings(classLoader: ClassLoader?) {
 
-        XposedHelpers.findAndHookMethod(Versions.settingPreference, classLoader, "onBindView", View::class.java, object : XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("com.tencent.mm.ui.base.MMListPopupWindow", classLoader, "setAdapter", ListAdapter::class.java, object : XC_MethodHook() {
             @Throws(Throwable::class)
-            override fun afterHookedMethod(param: MethodHookParam) {
-                val v = param.args[0] as View?
-                val title = XposedHelpers.callMethod(param.thisObject, "getTitle") as String?
-                if (title == "多彩微信") {
-                    v?.setOnClickListener {
-                        val inMain = Intent("com.rarnu.tophighlight.MAIN")
-                        inMain.addCategory(Intent.CATEGORY_DEFAULT)
-                        _ctx?.startActivity(inMain)
-                    }
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val count = XposedHelpers.callMethod(param.args[0], "getCount") as Int
+                if (count == 5) {
+                    // add menu item
+                    val ole = XposedHelpers.getObjectField(XposedHelpers.getObjectField(param.args[0], "olj"), "ole")
+                    val d = XposedHelpers.newInstance(XposedHelpers.findClass("com.tencent.mm.ui.t\$d", classLoader), 99, "多彩微信", "", 0x7f07027d, 0)
+                    val c = XposedHelpers.newInstance(XposedHelpers.findClass("com.tencent.mm.ui.t\$c", classLoader), d)
+                    XposedHelpers.callMethod(ole, "put", 5, c)
                 }
             }
         })
 
-        XposedHelpers.findAndHookMethod(Versions.settingActivity, classLoader, "onCreate", Bundle::class.java, object : XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("com.tencent.mm.ui.t\$a", classLoader, "getView", Integer.TYPE, View::class.java, ViewGroup::class.java, object : XC_MethodHook() {
             @Throws(Throwable::class)
             override fun afterHookedMethod(param: MethodHookParam) {
-                _ctx = param.thisObject as Context  // activity
-                val clsPreference = XposedHelpers.findClass(Versions.settingPreference, classLoader)
-                val objPreference = XposedHelpers.newInstance(clsPreference, _ctx)
-                XposedHelpers.callMethod(objPreference, "setTitle", "多彩微信")
-                val prefList = XposedHelpers.getObjectField(_ctx, Versions.settingListField)
-                XposedHelpers.callMethod(prefList, Versions.settingAddMethod, objPreference, 0)
-
+                val v = param.result as View
+                val txt = v.findViewById(0x7f1000dd) as TextView?
+                val ctx = XposedHelpers.getObjectField(XposedHelpers.getObjectField(param.thisObject, "olj"), "mContext") as Context?
+                if (txt?.text.toString() == "多彩微信") {
+                    v.setOnClickListener {
+                        val inMain = Intent("com.rarnu.tophighlight.MAIN")
+                        inMain.addCategory(Intent.CATEGORY_DEFAULT)
+                        ctx?.startActivity(inMain)
+                    }
+                }
             }
         })
     }
