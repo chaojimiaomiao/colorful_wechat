783e06c06b972ee7c8078922053c7d2be49d12f5
rarnu <hexiaojie@hujiang.com>
2017-02-26 00:32:54 +0800

    * try login

--- src/main/kotlin/com/rarnu/tophighlight/market/UserLoginRegisterActivity.kt
+++ src/main/kotlin/com/rarnu/tophighlight/market/UserLoginRegisterActivity.kt
@@ -1,19 +1,22 @@
 package com.rarnu.tophighlight.market
 
-import android.app.Activity
 import android.os.Bundle
-import android.view.MenuItem
+import android.os.Handler
+import android.os.Message
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RelativeLayout
+import android.widget.Toast
 import com.rarnu.tophighlight.R
+import com.rarnu.tophighlight.api.LocalApi
+import com.rarnu.tophighlight.api.WthApi
+import kotlin.concurrent.thread
 
 /**
  * Created by rarnu on 2/25/17.
  */
 class UserLoginRegisterActivity : BaseMarkerActivity(), View.OnClickListener {
-
 
     private var layLogin: RelativeLayout? = null
     private var layRegister: RelativeLayout? = null
@@ -70,8 +73,8 @@ class UserLoginRegisterActivity : BaseMarkerActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when(v.id) {
-            R.id.btnLogin -> { }
-            R.id.btnRegister -> { }
+            R.id.btnLogin -> login()
+            R.id.btnRegister -> register()
             R.id.btnSwitchRegister -> {
                 layRegister?.visibility = View.VISIBLE
                 layLogin?.visibility = View.GONE
@@ -79,6 +82,62 @@ class UserLoginRegisterActivity : BaseMarkerActivity(), View.OnClickListener {
             R.id.btnSwitchLogin -> {
                 layLogin?.visibility = View.VISIBLE
                 layRegister?.visibility = View.GONE
+            }
+        }
+    }
+
+    val hSucc = object : Handler() {
+        override fun handleMessage(msg: Message?) {
+            setResult(RESULT_OK)
+            finish()
+            super.handleMessage(msg)
+        }
+    }
+
+    val hFail = object : Handler() {
+        override fun handleMessage(msg: Message?) {
+            if (msg != null) {
+                Toast.makeText(this@UserLoginRegisterActivity, when (msg.what) {
+                    0 -> getString(R.string.toast_login_failed)
+                    1 -> getString(R.string.toast_register_failed)
+                    else -> ""
+                }, Toast.LENGTH_SHORT)
+                .show()
+            }
+
+            super.handleMessage(msg)
+        }
+    }
+
+    private fun register() {
+        val acc = regAccount?.text?.toString()
+        val pwd = regPassword?.text?.toString()
+        val repeatPwd = regRepeatPassword?.text?.toString()
+        val nickname = regNickname?.text?.toString()
+        val email = regEmail?.text?.toString()
+        if (acc == null || acc == "" || pwd == null || pwd == "" || repeatPwd == null || repeatPwd == "" || nickname == null || nickname == "" || email == null || email == "" || pwd != repeatPwd) return
+        thread {
+            val id = WthApi.userRegister(acc, pwd, nickname, email)
+            if (id != 0) {
+                LocalApi.userId = id
+                hSucc.sendEmptyMessage(0)
+            } else {
+                hFail.sendEmptyMessage(1)
+            }
+        }
+    }
+
+    private fun login() {
+        val acc = loginAccount?.text?.toString()
+        val pwd = loginPassword?.text?.toString()
+        if (acc == null || acc == "" || pwd == null || pwd == "") return
+        thread {
+            val user = WthApi.userLogin(acc, pwd)
+            if (user != null && user.id != 0) {
+                LocalApi.userId = user.id
+                hSucc.sendEmptyMessage(0)
+            } else {
+                hFail.sendEmptyMessage(0)
             }
         }
     }
