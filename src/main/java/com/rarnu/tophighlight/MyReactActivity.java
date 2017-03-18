04020bcdd278bb3d29cf7d28dce28ab284f85a74
翟冰洁 <372470125@qq.com>
2017-03-18 23:35:39 +0800

    no message

---
+++ src/main/java/com/rarnu/tophighlight/MyReactActivity.java
@@ -0,0 +1,13 @@
+package com.rarnu.tophighlight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyReactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_react);
+    }
+}
